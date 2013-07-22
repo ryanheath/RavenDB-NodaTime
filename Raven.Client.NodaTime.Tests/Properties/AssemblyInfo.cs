68ae87c9aa1b1211f367debcd1fefc850891ec3d
Matt Johnson <mj1856@hotmail.com>
2013-07-22 12:00:18 -0700

    Fix AssemblyInfo

--- Raven.Client.NodaTime.Tests/Properties/AssemblyInfo.cs
+++ Raven.Client.NodaTime.Tests/Properties/AssemblyInfo.cs
@@ -2,5 +2,3 @@
 
 [assembly: AssemblyTitle("Raven.Client.NodaTime.Tests")]
 [assembly: AssemblyDescription("Unit tests for the NodaTime extensions for the RavenDB client library.")]
-[assembly: AssemblyVersion("2.5.0.0")]
-[assembly: AssemblyFileVersion("2.5.0.0")]
---
+++ SharedAssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("Matt Johnson")]
+[assembly: AssemblyProduct("Noda Time Support for RavenDB")]
+[assembly: AssemblyCopyright("Copyright © 2012-2013 Matt Johnson")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("2.5.1")]
+[assembly: AssemblyFileVersion("2.5.1.0")]
+[assembly: AssemblyInformationalVersion("2.5.1-Unstable")]
