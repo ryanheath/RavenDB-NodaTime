5675143e7ea2b303f7d084ba73dd99d9e646647c
Matt Johnson <mj1856@hotmail.com>
2015-01-14 23:02:51 -0800

    Version Bump 2.5.10

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyCompany("Matt Johnson")]
 [assembly: AssemblyProduct("Noda Time Support for RavenDB")]
-[assembly: AssemblyCopyright("Copyright © 2012-2014 Matt Johnson")]
+[assembly: AssemblyCopyright("Copyright © 2012-2015 Matt Johnson")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("2.5.9.*")]
-[assembly: AssemblyInformationalVersion("2.5.9")]
+[assembly: AssemblyVersion("2.5.10.*")]
+[assembly: AssemblyInformationalVersion("2.5.10")]
