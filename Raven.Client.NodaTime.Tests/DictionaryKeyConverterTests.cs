5eb4a115575d0d7e4044f59f9c1ed5773b83ea6c
Matt Johnson <mj1856@hotmail.com>
2015-01-14 23:57:11 -0800

    Add unit tests for #4

---
+++ Raven.Client.NodaTime.Tests/DictionaryKeyConverterTests.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using NodaTime;
+using Raven.Tests.Helpers;
+using Xunit;
+
+namespace Raven.Client.NodaTime.Tests
+{
+    public class DictionaryKeyConverterTests : RavenTestBase
+    {
+        [Fact]
+        public void Can_Use_NodaTime_Instant_In_Dictionary_Key()
+        {
+            using (var documentStore = NewDocumentStore())
+            {
+                documentStore.ConfigureForNodaTime();
+
+                Instant i1 = SystemClock.Instance.Now;
+                Instant i2 = i1 + Duration.FromHours(1);
+
+                using (var session = documentStore.OpenSession())
+                {
+                    var testData = new Dictionary<Instant, string> {{i1, "i1"}, {i2, "i2"}};
+                    session.Store(new Foo { Id = "foos/1", TestData = testData});
+                    session.SaveChanges();
+                }
+
+                using (var session = documentStore.OpenSession())
+                {
+                    var foo = session.Load<Foo>("foos/1");
+
+                    Assert.Equal("i1", foo.TestData[i1]);
+                    Assert.Equal("i2", foo.TestData[i2]);
+                }
+            }
+        }
+
+        [Fact]
+        public void Can_Use_NodaTime_Instant_In_IDictionary_Key()
+        {
+            using (var documentStore = NewDocumentStore())
+            {
+                documentStore.ConfigureForNodaTime();
+
+                Instant i1 = SystemClock.Instance.Now;
+                Instant i2 = i1 + Duration.FromHours(1);
+
+                using (var session = documentStore.OpenSession())
+                {
+                    var testData = new Dictionary<Instant, string> { { i1, "i1" }, { i2, "i2" } };
+                    session.Store(new Bar { Id = "bars/1", TestData = testData });
+                    session.SaveChanges();
+                }
+
+                using (var session = documentStore.OpenSession())
+                {
+                    var bar = session.Load<Bar>("bars/1");
+
+                    Assert.Equal("i1", bar.TestData[i1]);
+                    Assert.Equal("i2", bar.TestData[i2]);
+                }
+            }
+        }
+
+        public class Foo
+        {
+            public string Id { get; set; }
+            public Dictionary<Instant,string> TestData { get; set; }
+        }
+
+        public class Bar
+        {
+            public string Id { get; set; }
+            public IDictionary<Instant, string> TestData { get; set; }
+        }
+    }
+}
