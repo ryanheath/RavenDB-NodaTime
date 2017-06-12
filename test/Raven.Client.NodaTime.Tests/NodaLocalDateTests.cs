f792be2352bd5b3117b8096bd5fa40d75f213dcf
Ryan Heath <ryan.q.heath@gmail.com>
2017-06-12 21:03:29 +0200

    Added RelaxedNodaPatternConverter, registered LocalDateConverter as RelaxedNodaPatternConverter

--- test/Raven.Client.NodaTime.Tests/NodaLocalDateTests.cs
+++ test/Raven.Client.NodaTime.Tests/NodaLocalDateTests.cs
@@ -33,11 +33,26 @@ namespace Raven.Client.NodaTime.Tests
         {
             using (var documentStore = NewDocumentStore())
             {
+                using (var session = documentStore.OpenSession())
+                {
+                    session.Store(new Foo { Id = "foos/1", LocalDate = ld });
+
+                    // save localdate as nodatime localdate
+                    session.SaveChanges();
+                }
+
                 documentStore.ConfigureForNodaTime();
 
                 using (var session = documentStore.OpenSession())
                 {
-                    session.Store(new Foo { Id = "foos/1", LocalDate = ld });
+                    var foo = session.Load<Foo>("foos/1");
+
+                    // we can read localdate saved as nodatime localdate
+                    Assert.Equal(ld, foo.LocalDate);
+
+                    session.Store(foo);
+
+                    // save duration as iso pattern
                     session.SaveChanges();
                 }
 
@@ -45,6 +60,7 @@ namespace Raven.Client.NodaTime.Tests
                 {
                     var foo = session.Load<Foo>("foos/1");
 
+                    // we can read localdate saved as iso pattern
                     Assert.Equal(ld, foo.LocalDate);
                 }
 
