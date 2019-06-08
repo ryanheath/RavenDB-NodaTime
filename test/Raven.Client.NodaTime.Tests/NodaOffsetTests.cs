b097b501f06e0fc780c97fc601d4cd790cabc9fc
Ryan Heath <ryan.q.heath@gmail.com>
2019-06-08 17:07:57 +0200

    renamed test property Offset to Offst, as RavenDB fails to query it

--- test/Raven.Client.NodaTime.Tests/NodaOffsetTests.cs
+++ test/Raven.Client.NodaTime.Tests/NodaOffsetTests.cs
@@ -39,7 +39,7 @@ namespace Raven.Client.NodaTime.Tests
             {
                 using (var session = documentStore.OpenSession())
                 {
-                    session.Store(new Foo { Id = "foos/1", Offset = offset });
+                    session.Store(new Foo { Id = "foos/1", Offst = offset });
                     session.SaveChanges();
                 }
 
@@ -47,7 +47,7 @@ namespace Raven.Client.NodaTime.Tests
                 {
                     var foo = session.Load<Foo>("foos/1");
 
-                    Assert.Equal(offset, foo.Offset);
+                    Assert.Equal(offset, foo.Offst);
                 }
 
                 using (var session = documentStore.OpenSession())
@@ -57,7 +57,7 @@ namespace Raven.Client.NodaTime.Tests
                     var json = (BlittableJsonReaderObject)command.Result.Results[0];
                     System.Diagnostics.Debug.WriteLine(json.ToString());
                     var expected = offset.ToTimeSpan().ToString("c");
-                    json.TryGet("Offset", out string value);
+                    json.TryGet("Offst", out string value);
                     Assert.Equal(expected, value);
                 }
             }
@@ -93,36 +93,82 @@ namespace Raven.Client.NodaTime.Tests
             {
                 using (var session = documentStore.OpenSession())
                 {
-                    session.Store(new Foo { Id = "foos/1", Offset = offset });
-                    session.Store(new Foo { Id = "foos/2", Offset = offset + Offset.FromHours(1) });
-                    session.Store(new Foo { Id = "foos/3", Offset = offset + Offset.FromHours(2) });
+                    session.Store(new Foo { Id = "foos/1", Offst = offset });
+                    session.Store(new Foo { Id = "foos/2", Offst = offset + Offset.FromHours(1) });
+                    session.Store(new Foo { Id = "foos/3", Offst = offset + Offset.FromHours(2) });
                     session.SaveChanges();
                 }
 
                 using (var session = documentStore.OpenSession())
                 {
                     var q1 = session.Query<Foo>().Customize(x => x.WaitForNonStaleResults())
-                                    .Where(x => x.Offset == offset);
+                                    .Where(x => x.Offst == offset);
                     var results1 = q1.ToList();
                     Assert.Single(results1);
 
                     var q2 = session.Query<Foo>().Customize(x => x.WaitForNonStaleResults())
-                                    .Where(x => x.Offset > offset)
-                                    .OrderByDescending(x => x.Offset);
+                                    .Where(x => x.Offst > offset)
+                                    .OrderByDescending(x => x.Offst);
                     var results2 = q2.ToList();
                     Assert.Equal(2, results2.Count);
-                    Assert.True(results2[0].Offset > results2[1].Offset);
+                    Assert.True(results2[0].Offst > results2[1].Offst);
 
                     var q3 = session.Query<Foo>().Customize(x => x.WaitForNonStaleResults())
-                                    .Where(x => x.Offset >= offset)
-                                    .OrderByDescending(x => x.Offset);
+                                    .Where(x => x.Offst >= offset)
+                                    .OrderByDescending(x => x.Offst);
                     var results3 = q3.ToList();
                     Assert.Equal(3, results3.Count);
-                    Assert.True(results3[0].Offset > results3[1].Offset);
-                    Assert.True(results3[1].Offset > results3[2].Offset);
+                    Assert.True(results3[0].Offst > results3[1].Offst);
+                    Assert.True(results3[1].Offst > results3[2].Offst);
                 }
             }
         }
+
+        //class Bar
+        //{
+        //    public string Id { get; set; }
+        //    public int Offset { get; set; }
+        //}
+
+        //[Fact]
+        //public void Can_Use_Offset_As_PropertyName()
+        //{
+        //    var offset = 50;
+
+        //    using (var documentStore = NewDocumentStore())
+        //    {
+        //        using (var session = documentStore.OpenSession())
+        //        {
+        //            session.Store(new Bar { Id = "foos/1", Offset = offset });
+        //            session.Store(new Bar { Id = "foos/2", Offset = offset + 1 });
+        //            session.Store(new Bar { Id = "foos/3", Offset = offset + 2 });
+        //            session.SaveChanges();
+        //        }
+
+        //        using (var session = documentStore.OpenSession())
+        //        {
+        //            var q1 = session.Query<Bar>().Customize(x => x.WaitForNonStaleResults())
+        //                            .Where(x => x.Offset == offset);
+        //            var results1 = q1.ToList();
+        //            Assert.Single(results1);
+
+        //            var q2 = session.Query<Bar>().Customize(x => x.WaitForNonStaleResults())
+        //                            .Where(x => x.Offset > offset)
+        //                            .OrderByDescending(x => x.Offset);
+        //            var results2 = q2.ToList();
+        //            Assert.Equal(2, results2.Count);
+        //            Assert.True(results2[0].Offset > results2[1].Offset);
+
+        //            var q3 = session.Query<Bar>().Customize(x => x.WaitForNonStaleResults())
+        //                            .Where(x => x.Offset >= offset)
+        //                            .OrderByDescending(x => x.Offset);
+        //            var results3 = q3.ToList();
+        //            Assert.Equal(3, results3.Count);
+        //            Assert.True(results3[0].Offset > results3[1].Offset);
+        //            Assert.True(results3[1].Offset > results3[2].Offset);
+        //        }
+        //    }
+        //}
 
         private void Can_Use_NodaTime_Offset_In_Dynamic_Index2(Offset offset)
         {
@@ -130,33 +176,33 @@ namespace Raven.Client.NodaTime.Tests
             {
                 using (var session = documentStore.OpenSession())
                 {
-                    session.Store(new Foo { Id = "foos/1", Offset = offset });
-                    session.Store(new Foo { Id = "foos/2", Offset = offset - Offset.FromHours(1) });
-                    session.Store(new Foo { Id = "foos/3", Offset = offset - Offset.FromHours(2) });
+                    session.Store(new Foo { Id = "foos/1", Offst = offset });
+                    session.Store(new Foo { Id = "foos/2", Offst = offset - Offset.FromHours(1) });
+                    session.Store(new Foo { Id = "foos/3", Offst = offset - Offset.FromHours(2) });
                     session.SaveChanges();
                 }
 
                 using (var session = documentStore.OpenSession())
                 {
                     var q1 = session.Query<Foo>().Customize(x => x.WaitForNonStaleResults())
-                                    .Where(x => x.Offset == offset);
+                                    .Where(x => x.Offst == offset);
                     var results1 = q1.ToList();
                     Assert.Single(results1);
 
                     var q2 = session.Query<Foo>().Customize(x => x.WaitForNonStaleResults())
-                                    .Where(x => x.Offset < offset)
-                                    .OrderBy(x => x.Offset);
+                                    .Where(x => x.Offst < offset)
+                                    .OrderBy(x => x.Offst);
                     var results2 = q2.ToList();
                     Assert.Equal(2, results2.Count);
-                    Assert.True(results2[0].Offset < results2[1].Offset);
+                    Assert.True(results2[0].Offst < results2[1].Offst);
                     
                     var q3 = session.Query<Foo>().Customize(x => x.WaitForNonStaleResults())
-                                    .Where(x => x.Offset <= offset)
-                                    .OrderBy(x => x.Offset);
+                                    .Where(x => x.Offst <= offset)
+                                    .OrderBy(x => x.Offst);
                     var results3 = q3.ToList();
                     Assert.Equal(3, results3.Count);
-                    Assert.True(results3[0].Offset < results3[1].Offset);
-                    Assert.True(results3[1].Offset < results3[2].Offset);
+                    Assert.True(results3[0].Offst < results3[1].Offst);
+                    Assert.True(results3[1].Offst < results3[2].Offst);
                 }
             }
         }
@@ -193,33 +239,33 @@ namespace Raven.Client.NodaTime.Tests
 
                 using (var session = documentStore.OpenSession())
                 {
-                    session.Store(new Foo { Id = "foos/1", Offset = offset });
-                    session.Store(new Foo { Id = "foos/2", Offset = offset + Offset.FromHours(1) });
-                    session.Store(new Foo { Id = "foos/3", Offset = offset + Offset.FromHours(2) });
+                    session.Store(new Foo { Id = "foos/1", Offst = offset });
+                    session.Store(new Foo { Id = "foos/2", Offst = offset + Offset.FromHours(1) });
+                    session.Store(new Foo { Id = "foos/3", Offst = offset + Offset.FromHours(2) });
                     session.SaveChanges();
                 }
 
                 using (var session = documentStore.OpenSession())
                 {
                     var q1 = session.Query<Foo, TestIndex>().Customize(x => x.WaitForNonStaleResults())
-                                    .Where(x => x.Offset == offset);
+                                    .Where(x => x.Offst == offset);
                     var results1 = q1.ToList();
                     Assert.Single(results1);
 
                     var q2 = session.Query<Foo, TestIndex>().Customize(x => x.WaitForNonStaleResults())
-                                    .Where(x => x.Offset > offset)
-                                    .OrderByDescending(x => x.Offset);
+                                    .Where(x => x.Offst > offset)
+                                    .OrderByDescending(x => x.Offst);
                     var results2 = q2.ToList();
                     Assert.Equal(2, results2.Count);
-                    Assert.True(results2[0].Offset > results2[1].Offset);
+                    Assert.True(results2[0].Offst > results2[1].Offst);
                     
                     var q3 = session.Query<Foo, TestIndex>().Customize(x => x.WaitForNonStaleResults())
-                                    .Where(x => x.Offset >= offset)
-                                    .OrderByDescending(x => x.Offset);
+                                    .Where(x => x.Offst >= offset)
+                                    .OrderByDescending(x => x.Offst);
                     var results3 = q3.ToList();
                     Assert.Equal(3, results3.Count);
-                    Assert.True(results3[0].Offset > results3[1].Offset);
-                    Assert.True(results3[1].Offset > results3[2].Offset);
+                    Assert.True(results3[0].Offst > results3[1].Offst);
+                    Assert.True(results3[1].Offst > results3[2].Offst);
                 }
             }
         }
@@ -232,33 +278,33 @@ namespace Raven.Client.NodaTime.Tests
 
                 using (var session = documentStore.OpenSession())
                 {
-                    session.Store(new Foo { Id = "foos/1", Offset = offset });
-                    session.Store(new Foo { Id = "foos/2", Offset = offset - Offset.FromHours(1) });
-                    session.Store(new Foo { Id = "foos/3", Offset = offset - Offset.FromHours(2) });
+                    session.Store(new Foo { Id = "foos/1", Offst = offset });
+                    session.Store(new Foo { Id = "foos/2", Offst = offset - Offset.FromHours(1) });
+                    session.Store(new Foo { Id = "foos/3", Offst = offset - Offset.FromHours(2) });
                     session.SaveChanges();
                 }
 
                 using (var session = documentStore.OpenSession())
                 {
                     var q1 = session.Query<Foo, TestIndex>().Customize(x => x.WaitForNonStaleResults())
-                                    .Where(x => x.Offset == offset);
+                                    .Where(x => x.Offst == offset);
                     var results1 = q1.ToList();
                     Assert.Single(results1);
 
                     var q2 = session.Query<Foo, TestIndex>().Customize(x => x.WaitForNonStaleResults())
-                                    .Where(x => x.Offset < offset)
-                                    .OrderBy(x => x.Offset);
+                                    .Where(x => x.Offst < offset)
+                                    .OrderBy(x => x.Offst);
                     var results2 = q2.ToList();
                     Assert.Equal(2, results2.Count);
-                    Assert.True(results2[0].Offset < results2[1].Offset);
+                    Assert.True(results2[0].Offst < results2[1].Offst);
                     
                     var q3 = session.Query<Foo, TestIndex>().Customize(x => x.WaitForNonStaleResults())
-                                    .Where(x => x.Offset <= offset)
-                                    .OrderBy(x => x.Offset);
+                                    .Where(x => x.Offst <= offset)
+                                    .OrderBy(x => x.Offst);
                     var results3 = q3.ToList();
                     Assert.Equal(3, results3.Count);
-                    Assert.True(results3[0].Offset < results3[1].Offset);
-                    Assert.True(results3[1].Offset < results3[2].Offset);
+                    Assert.True(results3[0].Offst < results3[1].Offst);
+                    Assert.True(results3[1].Offst < results3[2].Offst);
                 }
             }
         }
@@ -266,7 +312,7 @@ namespace Raven.Client.NodaTime.Tests
         public class Foo
         {
             public string Id { get; set; }
-            public Offset Offset { get; set; }
+            public Offset Offst { get; set; }
         }
 
         public class TestIndex : AbstractIndexCreationTask<Foo>
@@ -276,7 +322,7 @@ namespace Raven.Client.NodaTime.Tests
                 Map = foos => from foo in foos
                               select new
                               {
-                                  foo.Offset
+                                  foo.Offst
                               };
 
             }
