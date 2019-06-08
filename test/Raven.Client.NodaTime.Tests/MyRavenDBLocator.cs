8ac80f0c991822216d479ac4165dc592391fc53b
Ryan Heath <ryan.q.heath@gmail.com>
2019-06-08 15:56:25 +0200

    Refactored MyRavenDBLocator

--- test/Raven.Client.NodaTime.Tests/MyRavenDBLocator.cs
+++ test/Raven.Client.NodaTime.Tests/MyRavenDBLocator.cs
@@ -1,69 +1,9 @@
-﻿using System;
-using System.IO;
-using Raven.Client.Documents;
+﻿using Raven.Client.Documents;
 using Raven.TestDriver;
 
 namespace Raven.Client.NodaTime.Tests
 {
-    public class MyRavenDBLocator : RavenServerLocator
-    {
-        private string _serverPath;
-        private string _command = "dotnet";
-        private readonly string RavenServerName = "Raven.Server";
-        private string _arguments;
-
-        public override string ServerPath
-        {
-            get
-            {
-                if (string.IsNullOrEmpty(_serverPath) == false)
-                {
-                    return _serverPath;
-                }
-
-                var path = Environment.GetEnvironmentVariable("Raven_Server_Test_Path");
-                if (string.IsNullOrEmpty(path) == false)
-                {
-                    if (InitializeFromPath(path))
-                        return _serverPath;
-                }
-                //If we got here we didn't have ENV:RavenServerTestPath setup for us maybe this is a CI enviroement
-                path = Environment.GetEnvironmentVariable("Raven_Server_CI_Path");
-                if (string.IsNullOrEmpty(path) == false)
-                {
-                    if (InitializeFromPath(path))
-                        return _serverPath;
-                }
-                throw new FileNotFoundException($"Could not find {RavenServerName} anywhere on the device.");
-            }
-        }
-
-        private bool InitializeFromPath(string path)
-        {
-            if (Path.GetFileNameWithoutExtension(path) != RavenServerName)
-                return false;
-            var ext = Path.GetExtension(path);
-            if (ext == ".dll")
-            {
-                _serverPath = path;
-                _arguments = _serverPath;
-                return true;
-            }
-            if (ext == ".exe")
-            {
-                _serverPath = path;
-                _command = _serverPath;
-                _arguments = "--License.Eula.Accepted=true";
-                return true;
-            }
-            return false;
-        }
-
-        public override string Command => _command;
-        public override string CommandArguments => _arguments;
-    }
-
-    public class MyRavenTestDriver : RavenTestDriver<MyRavenDBLocator>
+    public class MyRavenTestDriver : RavenTestDriver
     {
         protected override void PreInitialize(IDocumentStore documentStore)
         {
