f2dba25067f8609e463dc35842bb281f06979ab9
Thijs Brobbel <t.brobbel@buddypayment.nl>
2020-09-12 19:21:12 +0200

    Add test for DateInterval for the static index

--- test/Raven.Client.NodaTime.Tests/NodaDateIntervalTests.cs
+++ test/Raven.Client.NodaTime.Tests/NodaDateIntervalTests.cs
@@ -2,6 +2,7 @@
 using NodaTime;
 using NodaTime.Text;
 using Raven.Client.Documents.Commands;
+using Raven.Client.Documents.Indexes;
 using Sparrow.Json;
 using Xunit;
 
@@ -92,10 +93,67 @@ namespace Raven.Client.NodaTime.Tests
             }
         }
 
+        [Fact]
+        public void Can_Use_NodaTime_DateInterval_In_Static_Index()
+        {
+            var today = NodaUtil.LocalDate.Today;
+            var start = today.PlusDays(-1);
+            var end = today.PlusDays(1);
+            var dateInterval1 = new DateInterval(start, end);
+            var dateInterval2 = new DateInterval(end, end.PlusDays(1));
+
+            using (var documentStore = NewDocumentStore())
+            {
+                documentStore.ExecuteIndex(new TestIndex());
+
+                using (var session = documentStore.OpenSession())
+                {
+                    session.Store(new Foo { Id = "foos/1", DateInterval = dateInterval1 });
+                    session.Store(new Foo { Id = "foos/2", DateInterval = dateInterval2 });
+                    session.SaveChanges();
+                }
+
+                using (var session = documentStore.OpenSession())
+                {
+                    var q1 = session.Query<Foo, TestIndex>()
+                                    .Customize(x => x.WaitForNonStaleResults())
+                                    .Where(x => x.DateInterval.Start == dateInterval1.Start && x.DateInterval.End == dateInterval1.End);
+                    var results1 = q1.ToList();
+                    Assert.Single(results1);
+
+                    var q2 = session.Query<Foo, TestIndex>()
+                                    .Customize(x => x.WaitForNonStaleResults())
+                                    .Where(x => x.DateInterval.Start <= today && x.DateInterval.End > today);
+                    var results2 = q2.ToList();
+                    Assert.Single(results2);
+
+                    var q3 = session.Query<Foo, TestIndex>()
+                                    .Customize(x => x.WaitForNonStaleResults())
+                                    .OrderByDescending(x => x.DateInterval.Start);
+                    var results3 = q3.ToList();
+                    Assert.Equal(2, results3.Count);
+                    Assert.True(results3[0].DateInterval.Start > results3[1].DateInterval.Start);
+                }
+            }
+        }
+
         public class Foo
         {
             public string Id { get; set; }
             public DateInterval DateInterval { get; set; }
+        }
+
+        public class TestIndex : AbstractIndexCreationTask<Foo>
+        {
+            public TestIndex()
+            {
+                Map = foos => from foo in foos
+                              select new
+                              {
+                                  DateInterval_Start = foo.DateInterval.Start,
+                                  DateInterval_End = foo.DateInterval.End
+                              };
+            }
         }
     }
 }
