fe211d943ff735157d9b9cc964bd02df458ee2cc
Rob Scott <rob.scott@ungerboeck.com>
2025-02-05 15:03:35 -0500

    Bumped Raven client to 6.2.3. Bumped NodaTime to 3.2.1. Updated test driver to handle new licensing default.

--- test/Raven.Client.NodaTime.Tests/MyRavenTestDriver.cs
+++ test/Raven.Client.NodaTime.Tests/MyRavenTestDriver.cs
@@ -5,10 +5,26 @@ namespace Raven.Client.NodaTime.Tests
 {
     public class MyRavenTestDriver : RavenTestDriver
     {
+        private static bool _hasBeenConfigured;
+        
+        protected MyRavenTestDriver()
+        {
+            if (_hasBeenConfigured) 
+                return;
+            
+            ConfigureServer(new TestServerOptions
+            {
+                Licensing =
+                {
+                    ThrowOnInvalidOrMissingLicense = false
+                }
+            });
+            _hasBeenConfigured = true;
+        }
+
         protected override void PreInitialize(IDocumentStore documentStore)
         {
             documentStore.ConfigureForNodaTime();
-
             base.PreInitialize(documentStore);
         }
 
